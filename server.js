71d981d5f67692d270baf273f7128bb793e3b7af
shravani7115kamble-dev <shravani7115kamble@gmail.com>
2026-03-22 19:53:51 +0530

    Create server.js as the main Express backend application entry point

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the QP Generator API!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
